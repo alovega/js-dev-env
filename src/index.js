7aac6c589e4cdcdbe4016974b8c4d4bfd7063360
alovega <alovegakevin@gmail.com>
2019-07-23 13:26:09 +0300

    http requests

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import '../src/index.css'
 
-import numeral from 'numeral';
-const courseValue = numeral(1000).format('sh0,0.00')
+import {getUsers} from './api/userApi';
 
-console.log(`I would pay ${courseValue} for this awesome course`) //eslint-disable-line no-console
+//populate table of users via API call.
+getUsers().then(result => {
+    let usersBody = "";
+
+    result.forEach(user => {
+        usersBody+=`<tr>
+            <td><a href="#" data-id="${user.id}" class="deleteUser">delete</a></td>
+            <td>${user.id}</td>
+            <td>${user.firstName}</td>
+            <td>${user.lastName}</td>
+            <td>${user.email}</td>
+        </tr>`
+    });
+
+    global.document.getElementById('users').innerHTML = usersBody;
+});

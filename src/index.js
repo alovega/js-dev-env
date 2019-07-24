160d32fdfdd831af8452d40c1f7fc2cb629c8251
alovega <alovegakevin@gmail.com>
2019-07-24 14:33:18 +0300

    mock api's set up

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import '../src/index.css'
 
-import {getUsers} from './api/userApi';
+import {getUsers, deleteUser} from './api/userApi';
 
 //populate table of users via API call.
 getUsers().then(result => {
@@ -17,4 +17,19 @@ getUsers().then(result => {
     });
 
     global.document.getElementById('users').innerHTML = usersBody;
+
+    const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+    // Must use array.from to create a real array from DOM collection.
+    // getElementByClassName only returns array like objects
+
+    Array.from(deleteLinks, link => {
+        link.onclick = function(event){
+            const element = event.target;
+            event.preventDefault();
+            deleteUser(element.attributes["data-id"].value)
+            const row = element.parentNode.parentNode
+            row.parentNode.removeChild(row)
+        }
+    });
 });
